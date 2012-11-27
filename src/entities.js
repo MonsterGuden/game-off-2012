9a9c78b23723d38672116b2d7702a27cbd806d9a
MonsterGuden <monsterguden@gmail.com>
2012-11-27 15:15:11 +0100

    the autojump is removed

--- src/entities.js
+++ src/entities.js
@@ -11,26 +11,23 @@ var PlayerEntity = me.ObjectEntity.extend({
 	me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
     },
     update: function() {
+        // get all collisions
+	var res = me.game.collide(this);
+
+        if(res && res.obj.type == "game_complete")
+            me.state.change(me.state.GAME_END);
+
 	if(me.input.isKeyPressed("jump")) {
 	    if(!this.falling && !this.jumping) {
-		this.vel.y = -this.jumpVelocity * me.timer.tick;
+                if (res && res.obj.type == "jumper")
+                    this.vel.y = -this.maxVel.y;
+                else
+		    this.vel.y = -this.jumpVelocity * me.timer.tick;
 		this.jumping = true;
 	    }
 	}
 	this.vel.x += this.accel.x * me.timer.tick;
 	this.updateMovement();
-
-	var res = me.game.collide(this);
-	if (res) {
-	    if (res.obj.type == "jumper") {
-		this.vel.y = -this.maxVel.y;
-		this.jumping = true;
-		this.updateMovement();
-	    }
-	    else if(res.obj.type == "game_complete") {
-		me.state.change(me.state.GAME_END);
-	    }
-	}
 	this.parent(true);
 	return true;
     }
@@ -43,11 +40,9 @@ var JumperEntity = me.ObjectEntity.extend({
 	this.type = JUMPER;
 	this.collidable = true;
 	this.type = "jumper";
-	this.updateColRect(-1, 0, 32, 32);
+	this.updateColRect(11, 10, -1, 0);
     },
     update: function() {
-	this.parent(true);
-	true;
     }
 });
 
